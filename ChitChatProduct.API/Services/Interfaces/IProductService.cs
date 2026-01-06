74fa14ac645663a5fc7fa72c966bbb957fd08167
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 12:50:35 +0600

    feat: create service interface for product

---
+++ ChitChatProduct.API/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ChitChatProduct.API.DTOs;
+using ChitChatProduct.API.DTOs.Product;
+
+namespace ChitChatProduct.API.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<APIResponse> CreateProductAsync(CreateProductDto product);
+        Task<APIResponse> GetAllProductsAsync();
+        Task<APIResponse> GetProductByIdAsync(int id);
+        Task<APIResponse> UpdateProductAsync(int id, UpdateProductDto product);
+        Task<APIResponse> DeleteProductAsync(int id);
+    }
+}

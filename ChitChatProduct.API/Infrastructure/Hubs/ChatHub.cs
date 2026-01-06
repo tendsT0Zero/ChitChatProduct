4d8a80ec0d6055ac3d5a24ffe569f7d8f7871306
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 16:55:10 +0600

    feat: Create chathub

---
+++ ChitChatProduct.API/Infrastructure/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChitChatProduct.API.Infrastructure.Hubs
+{
+    public class ChatHub : Hub
+    {
+        
+        public async Task JoinConversation(string conversationId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conversationId);
+        }
+
+        
+        public async Task LeaveConversation(string conversationId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, conversationId);
+        }
+    }
+}

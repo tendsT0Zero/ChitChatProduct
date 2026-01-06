f721fada4f196e9891154f70da66a18b66d26818
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 15:24:17 +0600

    feat:dto for chat message model

---
+++ ChitChatProduct.API/DTOs/Message/MessageResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ChitChatProduct.API.DTOs.Message
+{
+    public class MessageResponseDto
+    {
+        public long Id { get; set; }
+        public int SenderId { get; set; }
+        public string SenderName { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
---
+++ ChitChatProduct.API/DTOs/Message/SendMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ChitChatProduct.API.DTOs.Message
+{
+    public class SendMessageDto
+    {
+        public int ConversationId { get; set; }
+        public int SenderId { get; set; }
+        public string Content { get; set; }
+    }
+}

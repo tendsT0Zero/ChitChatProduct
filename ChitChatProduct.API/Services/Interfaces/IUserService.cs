e03dca63e1c86fb184a2db3393542f6329e47f6d
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 12:04:39 +0600

    feat:add crud services in IUserService interface

---
+++ ChitChatProduct.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ChitChatProduct.API.DTOs;
+using ChitChatProduct.API.DTOs.User;
+
+namespace ChitChatProduct.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<APIResponse> CreateUserAsync(CreateUserDto user);
+        Task<APIResponse> UpdateUserAsync(int userId,UpdateUserDto user);
+        Task<APIResponse> DeleteUserAsync(int id);
+        Task<APIResponse> GetAllUserAsync();
+        Task<APIResponse> GetUserAsync(int id);
+
+    }
+}

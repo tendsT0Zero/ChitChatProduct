03efa595a93748070c5d9b4863b7d55b9b840c0f
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 12:51:40 +0600

    feat: establish api endpoints for product

---
+++ ChitChatProduct.API/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using ChitChatProduct.API.DTOs;
+using ChitChatProduct.API.DTOs.Product;
+using ChitChatProduct.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChitChatProduct.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto createProductDto)
+        {
+            if (createProductDto == null)
+            {
+                return BadRequest(new APIResponse
+                {
+                    IsSuccess = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "Invalid client request"
+                });
+            }
+
+            var response = await _productService.CreateProductAsync(createProductDto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await _productService.GetAllProductsAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest(new APIResponse
+                {
+                    IsSuccess = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "Invalid Product ID"
+                });
+            }
+
+            var response = await _productService.GetProductByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] UpdateProductDto updateProductDto)
+        {
+            if (updateProductDto == null)
+            {
+                return BadRequest(new APIResponse
+                {
+                    IsSuccess = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "Invalid client request"
+                });
+            }
+
+            var response = await _productService.UpdateProductAsync(id, updateProductDto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(APIResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest(new APIResponse
+                {
+                    IsSuccess = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "Invalid Product ID"
+                });
+            }
+
+            var response = await _productService.DeleteProductAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}

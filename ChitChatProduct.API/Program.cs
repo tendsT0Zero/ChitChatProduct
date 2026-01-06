843d4aafc7a6c482e8957d25295a3631314b3d33
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 12:38:47 +0600

    chores: configure swagger

--- ChitChatProduct.API/Program.cs
+++ ChitChatProduct.API/Program.cs
@@ -25,13 +25,14 @@ builder.Services.AddControllers()
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

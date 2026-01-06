bd33ca3bd5bafdc451c2855bd933cade4a2e5cae
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 16:55:46 +0600

    refact:change datatype for status code

--- ChitChatProduct.API/DTOs/APIResponse.cs
+++ ChitChatProduct.API/DTOs/APIResponse.cs
@@ -3,7 +3,7 @@
     public class APIResponse
     {
         public object ResponseObject { get; set; } = null;
-        public string StatusCode { get; set; }
+        public int StatusCode { get; set; }
         public bool IsSuccess { get; set; }=false;
         public string Message { get; set;  }=string.Empty;
 

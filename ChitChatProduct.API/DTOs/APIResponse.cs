778fea3104099242af72afb49135b33e3c2d2457
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 11:00:55 +0600

    "feat(dto): add standard API response DTO"

---
+++ ChitChatProduct.API/DTOs/APIResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ChitChatProduct.API.DTOs
+{
+    public class APIResponse
+    {
+        public object ResponseObject { get; set; } = null;
+        public bool IsSuccess { get; set; }=false;
+        public string Message { get; set;  }=string.Empty;
+
+    }
+}

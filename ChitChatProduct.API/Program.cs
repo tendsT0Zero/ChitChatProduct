e3b24f610e6631aa156c4f5d56d4a796afec13a3
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-06 10:45:56 +0600

    feat:establish db connection.

--- ChitChatProduct.API/Program.cs
+++ ChitChatProduct.API/Program.cs
@@ -1,7 +1,13 @@
+using ChitChatProduct.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
